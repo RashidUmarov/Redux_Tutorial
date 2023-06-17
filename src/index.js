f169f6c039d43c6bee957b714a0348aaf1cab48d
Rashid Umarov <rasrasras2014@mail.ru>
2023-06-17 20:11:36 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
+
+import ReduxApp from "./components/App_redux";
+import {Provider} from "react-redux";
+import reducer from './redux/reducers';
+import {logging} from "./redux/midllewares/logging";
+// import App from "./components/App";
+
+import {createStore, applyMiddleware, compose} from 'redux';
+
+
+const store = createStore(reducer,
+    compose(applyMiddleware(logging),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+
+
+//
+// console.log('store',store);
+// console.log('2',store.getState());
+//
+// const testButton=document.querySelector(".testButton");
+// const items=document.querySelector(".testUL");
+// const inputValue=document.querySelector(".testInput");
+//
+// store.subscribe(()=>{
+//     console.log('subscribe',store.getState());
+//
+//     items.innerHTML='';
+//     inputValue.value='';
+//     store.getState().map(item=>{
+//         const li=document.createElement('li');
+//         li.textContent=item;
+//         items.append(li);
+//     })
+// })
+
+//
+// store.dispatch({type:'WRITE',payload:'OYEEE'})
+
+
+// testButton.addEventListener('click',()=>{
+//     console.log('INPUT',inputValue.value);
+//     store.dispatch({type:'WRITE',payload:inputValue.value});
+// })
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <Provider store={store}>
+        <ReduxApp/>,
+    </Provider>);
+
+// ReactDOM.render(
+//     <Provider store={store}>
+//         <ReduxApp/>,
+//     </Provider>,
+//     document.getElementById('root'));
